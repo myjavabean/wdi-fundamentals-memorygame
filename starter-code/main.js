975bc2eab9960dbbd69b11937c414781f46e68ad
challenge4better@gmail.com <lemonissweet@Meng-Lis-MacBook-Air.local>
2017-01-28 09:49:09 -0800

    Initial Add Memory Game codes

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,5 @@
-console.log("JS file is connected to HTML! Woo!")
+//console.log("JS file is connected to HTML! Woo!")
+var cardOne = 'Queen';
+var cardTwo = 'Queen';
+var cardThree = 'King', cardFour = 'King';
+
